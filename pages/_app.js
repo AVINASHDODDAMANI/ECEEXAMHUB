5d26a90a7e6f20b55a9822f71970c47810f24e8f
AVINASH DODDAMANI <avinash08121998@gmail.com>
2026-05-05 21:13:06 +0530

    UI UPDATE

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,10 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="theme-color" content="#123b79" />
         <meta
+          name="google-site-verification"
+          content="jhFrRO_BMQtPKAZBDFQyUiyjIu7kLsHh7RO7ovnXYkc"
+        />
+        <meta
           name="format-detection"
           content="telephone=no, date=no, email=no, address=no"
         />
